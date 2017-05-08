3ee43680d02f4216960a34fe49ef1b4d52bd6edf
Mac-nnnyyy <MapleMac@MapleMac.local>
2017-05-08 18:06:27 +0900

    Mac용 핫커뮤니티 세팅

---
+++ CommunityViewerExForiOS/CommunityViewerExForiOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CommunityViewerExForiOS
+//
+//  Created by MapleMac on 2017. 5. 8..
+//  Copyright © 2017년 MapleMac. All rights reserved.
+//
+
+import UIKit
+import SwiftHTTP
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
